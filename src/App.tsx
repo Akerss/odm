ba01846d1c6de02ff849cc0342c41b711b0f6184
Akerss <873503038@qq.com>
2022-05-24 23:38:16 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "antd/dist/antd.css"
+import { Button } from "antd"
+
+function App() {
+  return (
+    <div className="App">
+      Akerss.
+      <Button type="primary">Button</Button>
+      <Button type='link'>link</Button>
+    </div>
+  );
+}
+
+export default App;
